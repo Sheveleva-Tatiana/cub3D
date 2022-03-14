a93eac3b4b6a08eaf3d98a02c9c02e31f7480b49
Ilnur <ilnrzakirov@gmail.com>
2022-03-14 13:05:15 +0300

    create cub3d.h and libft

---
+++ Makefile
@@ -0,0 +1,37 @@
+NAME	=	minishell
+
+SRCS	=	src/main.c\
+
+LIB 	=	./lib/libft.a
+
+HEADER	=	includes/cub3d.h ./lib/libft.h
+
+RM		=	rm -f
+
+OBJS	=	$(SRCS:.c=.o)
+
+GCC		= clang
+
+CFLAGS	=	-Wall -Wextra -Werror
+
+FLAG	=	-g
+
+$(NAME)	:	$(OBJS) $(HEADER)
+			@$(MAKE) -C ./lib
+			@$(GCC) $(CFLAGS) $(OBJS) $(LIB) $(FLAG) -o ${NAME}
+			@echo "\033[0;32m-----Successful success!-----"
+
+.PHONY	:	all clean fclean re
+
+all		:	$(NAME)
+
+clean	:
+			@$(RM) $(OBJS)
+			@$(MAKE) clean -C ./lib
+			@echo "\033[0;32m-------Clean completed-------"
+
+fclean	:	clean
+			@$(RM) $(NAME)
+			@echo "\033[0;32m------Fclean completed-------"
+
+re		:	fclean all
---
+++ includes/cub3d.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshera <sshera@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 11:32:25 by bcarlee           #+#    #+#             */
+/*   Updated: 2022/02/28 16:02:46 by sshera           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <string.h>
+# include <errno.h>
+# include <fcntl.h>
+# include "../lib/libft.h"
+
+#endif
