6c845439a980472c8cc13b650f41624ebe3cf447
bcarlee <bcarlee@student.21-school.ru>
2022-03-19 15:09:06 +0300

    check map

--- src/map/valid_map2.c
+++ src/map/valid_map2.c
@@ -1,6 +1,41 @@
 #include "../../include/cub3d.h"
 
+void	close_wall_in_utils(int i, t_data *data, int *err, int j)
+{
+	if (i != (size_arr(data->map->map) - 1) && !data->map->map[i + 	1][j])
+	{
+		if (!ft_strchr("1\n", data->map->map[i][j]))
+			*err = 1;
+	}
+	if (i != (size_arr(data->map->map) - 1) && data->map->map[i + 1][j])
+	{
+		if (data->map->map[i + 1][j] == '\n' || data->map->map[i + 1][j] ==
+		'\0')
+			if (!ft_strchr("1\n", data->map->map[i][j]))
+				*err = 1;
+	}
+}
+
 void	check_close_wall_inside(t_data *data)
 {
+	int	i;
+	int j;
+	int err;
 
+	i = 0;
+	err = 0;
+	while(data->map->map[i])
+	{
+		j = 0;
+		while (data->map->map[i][j]) {
+			close_wall_in_utils(i, data, &err, j);
+			j++;
+		}
+		i++;
+	}
+	if (err == 1)
+	{
+		printf("Error\n");
+		clear_and_exit(data);
+	}
 }
